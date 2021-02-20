5856268d48de8a5980b9169237a688a9883cbd11
Sara Strong <sara.n.strong@gmail.com>
2021-02-19 22:48:57 -0500

    Documentation for sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,14 +1,26 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Formats data for use in a SQL update
+ *
+ * Args:
+ *
+ * dataToUpdate
+ *    {firstName: "Hedy", lastName: "Lamarr"}
+ *
+ * jsToSql
+ * (used to ensure column names are consistent with how they're formatted in the database (snake_case) rather than JS (camelCase) wherever that might be an issue)
+ *   {firstName: "first_name", lastName: "last_name", "isAdmin": "is_admin"}
+ *
+ * Returns {setCols: "first_name, last_name", values: ["Hedy", "Lamarr"]}
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  const cols = keys.map(
+    (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
   );
 
   return {
