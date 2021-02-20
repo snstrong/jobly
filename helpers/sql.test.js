303c74ee1e2a6fb6dece0762f04b7f3743606fb4
Sara Strong <sara.n.strong@gmail.com>
2021-02-19 22:59:16 -0500

    Test sqlForPartialUpdate handles bad request

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,7 +1,8 @@
+const { BadRequestError } = require("../expressError");
 const { sqlForPartialUpdate } = require("./sql");
 
 describe("sqlForPartialUpdate", function () {
-  test("handles good request", function () {
+  test("works: handles good request", function () {
     let data = { firstName: "Hedy", lastName: "Lamarr" };
     let colNames = { firstName: "first_name", lastName: "last_name" };
     let result = sqlForPartialUpdate(data, colNames);
@@ -9,5 +10,12 @@ describe("sqlForPartialUpdate", function () {
       setCols: '"first_name"=$1, "last_name"=$2',
       values: ["Hedy", "Lamarr"],
     });
+  });
+  test("works: handles bad request (no data)", function () {
+    try {
+      sqlForPartialUpdate({}, {});
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
   });
 });
