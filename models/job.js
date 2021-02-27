faf12a0b032bd913ba049f8bd3ab1243d61a3d27
Sara Strong <sara.n.strong@gmail.com>
2021-02-27 17:28:29 -0500

    Job.update

--- models/job.js
+++ models/job.js
@@ -73,6 +73,39 @@ class Job {
     return job;
   }
 
+  /** Update job data with `data`.
+   *
+   * This is a "partial update" --- it's fine if data doesn't contain all the
+   * fields; this only changes provided ones.
+   *
+   * Data can include: {title, salary, equity, companyHandle}
+   *
+   * Returns {id, title, salary, equity, company_handle}
+   *
+   * Throws NotFoundError if not found.
+   */
+  static async update(id, data) {
+    const { setCols, values } = sqlForPartialUpdate(data, {
+      companyHandle: "company_handle",
+    });
+    const handleVarIdx = "$" + (values.length + 1);
+
+    const querySql = `UPDATE jobs 
+                      SET ${setCols} 
+                      WHERE id = ${handleVarIdx} 
+                      RETURNING id,
+                                title,
+                                salary,
+                                equity,
+                                company_handle AS "companyHanle"`;
+    const result = await db.query(querySql, [...values, id]);
+    const job = result.rows[0];
+
+    if (!job) throw new NotFoundError(`No company: ${id}`);
+
+    return job;
+  }
+
   /** Delete given job from database; returns undefined.
    *
    * Throws NotFoundError if job not found.
